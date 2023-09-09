e29a6bdf82c2d459c1fd923bb8267e7f5e75035e
Arijit <bhowmikzar@gmail.com>
2023-09-09 20:38:49 +0530

    Prompts section responsiveness still in progress

--- components/LandingPageComponents/Sections/Prompts/index.tsx
+++ components/LandingPageComponents/Sections/Prompts/index.tsx
@@ -25,34 +25,32 @@ const Prompts = () => {
       controls.start("visible");
     }
   }, [controls, inView]);
-
   useEffect(() => {
     if (inView2) {
       controls2.start("visible");
     }
   }, [controls2, inView2]);
-
   return (
     <div className="pb-10">
-      <div className="bg-white md:h-[500px] h-[600px] flex md:px-10 px-4 md:mr-[3%]">
+      <div className="bg-white h-[500px] flex md:px-10 px-16 mr-[3%]">
         <motion.div
           ref={ref}
           variants={textScrollVariants}
           initial="hidden"
-          className="w-full md:w-[50%] flex flex-col gap-y-5 justify-center relative right-0 md:right-[-200px] transition-all duration-1000 ease-in-out"
+          className="w-[50%] flex flex-col gap-y-5 justify-center relative right-[-200px] transition-all duration-1000 ease-in-out"
           animate={controls}
         >
-          <div className="md:hidden flex justify-center">
+          <div className="md:hidden flex justify-center  ">
             <Image
-              className="w-[100%] md:w-[678px] h-[180px]"
+              className=" w-[678px] h-[180px] "
               src={RImage1}
               alt="Preview"
             />
           </div>
-          <h1 className="text-[#3247CF] font-semibold md:text-[48px] text-4xl leading-[48px] md:w-[380px] w-full">
+          <h1 className="text-[#3247CF] font-semibold md:text-[48px] text-4xl leading-[48px] w-[380px]">
             No More Bad Prompts, Only Great Ideas
           </h1>
-          <p className="font-normal text-[16px] leading-[18.75px] text-justify text-[#494949] md:w-[387px] w-full">
+          <p className="font-normal text-[16px] leading-[18.75px] text-justify text-[#494949] w-[387px]">
             Metridash makes content generation easy. Just tell us a bit about
             yourself and your audience, and we will create a customized prompt
             that guarantees top-notch results instantly.
@@ -62,36 +60,36 @@ const Prompts = () => {
           ref={ref}
           variants={textScrollVariants}
           initial="hidden"
-          className="w-full md:w-[50%] mt-[5%] flex relative justify-center md:mr-10 right-0 md:right-[200px] transition-all duration-1000 ease-in-out"
+          className="w-[50%] mt-[5%] flex relative justify-center mr-10 right-[200px] transition-all duration-1000 ease-in-out"
           animate={controls}
         >
           <Image
-            className="absolute w-[100%] md:w-[329px] h-[333px] md:flex hidden object-contain"
+            className="absolute w-[329px] h-[333px] md:flex hidden object-contain"
             src={Image1}
             alt="Preview"
           />
           <Image
-            className="absolute w-[100%] md:w-[238px] h-[209px] md:ml-[54%] md:mt-[25%] mt-[5%] ml-[10%] md:flex hidden object-contain"
+            className="absolute w-[238px] h-[209px] ml-[54%] mt-[25%] md:flex hidden object-contain"
             src={Image2}
             alt="Preview"
           />
         </motion.div>
       </div>
-      <div className="bg-white md:h-[500px] h-[600px] flex md:pl-[7%] md:mr-[3%] md:flex-row flex-col">
+      <div className="bg-white md:h-[500px] h-[600px] md:px-10 flex pl-[7%] mr-[3%] md:flex-row flex-col">
         <motion.div
-          ref={ref2}
+          ref={ref}
           variants={textScrollVariants}
           initial="hidden"
-          className="w-full md:w-[50%] mt-[5%] flex relative justify-center md:mr-10 right-0 md:right-[200px] transition-all duration-1000 ease-in-out"
-          animate={controls2}
+          className="w-[50%] mt-[5%] flex relative justify-center mr-10 right-[200px] transition-all duration-1000 ease-in-out"
+          animate={controls}
         >
           <Image
-            className="absolute w-[100%] md:w-[329px] h-[333px] md:flex hidden object-contain"
+            className="absolute w-[329px] h-[333px] md:flex hidden object-contain"
             src={Image3}
             alt="Preview"
           />
           <Image
-            className="absolute w-[100%] md:w-[238px] h-[209px] md:ml-[54%] md:mt-[25%] mt-[5%] ml-[10%] md:flex hidden object-contain"
+            className="absolute w-[238px] h-[209px] ml-[54%] mt-[25%] md:flex hidden object-contain"
             src={Image4}
             alt="Preview"
           />
@@ -101,18 +99,18 @@ const Prompts = () => {
           ref={ref2}
           variants={textScrollVariants}
           initial="hidden"
-          className="w-full md:w-[50%] py-4 flex flex-col gap-y-5 justify-center relative right-0 md:right-[-50px] transition-all duration-1000 ease-in-out"
+          className="w-[50%] py-4 flex flex-col gap-y-5 justify-center relative right-[-50px] transition-all duration-1000 ease-in-out"
           animate={controls2}
         >
           <Image
-            className="w-[100%] md:w-[278px] h-[209px] md:ml-[10.5%] md:mt-[11.8%] mt-[5%] ml-[10%] md:hidden flex"
+            className=" w-[278px] h-[209px] ml-[10.5%] mt-[11.8%] md:hidden flex"
             src={RImage5}
             alt="Preview"
           />
-          <h1 className="text-[#3247CF] font-semibold md:text-[48px] text-4xl leading-[48px] md:w-[380px] w-full">
+          <h1 className="text-[#3247CF]  font-semibold md:text-[48px] text-4xl leading-[48px] w-[380px]">
             Focus on Your Growth, We Handle the Rest
           </h1>
-          <p className="font-normal text-[16px] leading-[18.75px] text-justify text-[#494949] md:w-[387px] w-full align-center">
+          <p className="font-normal text-[16px] leading-[18.75px] text-justify text-[#494949] w-[387px] align-center">
             Metridash does the content generation work for you, so you can
             invest more time in your strategies and business growth. We will
             speed up content idea creation, letting you focus on the important
