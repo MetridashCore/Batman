f9582bbe6fa5ebd33ecf412070f47901e64935c5
Mr. Robot <gomexxxx71@gmail.com>
2023-11-10 22:49:04 +0600

    perf: improve registration

--- auth.js
+++ auth.js
@@ -5,7 +5,7 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
   signOut,
-} from "firebase/auth"
+} from 'firebase/auth'
 import {
   getFirestore,
   doc,
@@ -17,13 +17,14 @@ import {
   query,
   where,
   arrayUnion,
-} from "firebase/firestore"
-import { db } from "./firebase"
-import { updateDoc } from "firebase/firestore"
-import Alert from "@mui/material/Alert"
+  runTransaction,
+} from 'firebase/firestore'
+import { db } from './firebase'
+import { updateDoc } from 'firebase/firestore'
+import Alert from '@mui/material/Alert'
 
 // import { db } from './firebaseConfig';
-import { auth } from "./firebase"
+import { auth } from './firebase'
 // const auth = getAuth(app)
 
 const firestore = db
@@ -31,7 +32,7 @@ const firestore = db
 const googleProvider = new GoogleAuthProvider()
 
 export const readTokens = async (user) => {
-  const userRef = doc(firestore, "users", user.uid)
+  const userRef = doc(firestore, 'users', user.uid)
   const userDoc = await userRef.get()
   const userData = userDoc.data()
   if (userData) {
@@ -42,7 +43,7 @@ export const readTokens = async (user) => {
 
 export const getRealTimeToken = async (user) => {
   let value
-  const tokenRef = doc(firestore, "users", user.uid)
+  const tokenRef = doc(firestore, 'users', user.uid)
   await onSnapshot(tokenRef, (snapshot) => {
     value = snapshot.data().tokens
   })
@@ -60,7 +61,7 @@ export const generateRealTimeToken = (user) => {
   // Get the current user's token
   if (!user) return null
   let token = getUserToken(user)
-  const tokenRef = doc(firestore, "users", user.uid)
+  const tokenRef = doc(firestore, 'users', user.uid)
   // Listen for changes to the token
   onSnapshot(tokenRef, (newToken) => {
     // Update the token if it has changed
@@ -76,7 +77,7 @@ export const generateRealTimeToken = (user) => {
 export const getUserToken = async (user) => {
   if (!user) return null
 
-  const tokenRef = doc(firestore, "users", user.uid)
+  const tokenRef = doc(firestore, 'users', user.uid)
   const doc1 = await getDoc(tokenRef)
 
   if (doc1.exists) {
@@ -87,40 +88,43 @@ export const getUserToken = async (user) => {
 }
 
 export const updateTokens = async (user, newTokenValue) => {
-  const userRef = doc(firestore, "users", user.uid)
+  const userRef = doc(firestore, 'users', user.uid)
 
   await updateDoc(userRef, { tokens: newTokenValue })
 }
 
 export const updateModel = async (user, newModelValue) => {
-  const userRef = doc(firestore, "users", user.uid)
+  const userRef = doc(firestore, 'users', user.uid)
   await updateDoc(userRef, { model: newModelValue })
 }
 export const addDraft = async (user, data, platform) => {
-  const userRef = doc(db, "users", user.uid)
-  const newObject = {draft:data, platform: user}
-  const CurrentDate = new Date();
-  
- 
+  const userRef = doc(db, 'users', user.uid)
+  const newObject = { draft: data, platform: user }
+  const CurrentDate = new Date()
+
   try {
     const userDoc = await getDoc(userRef)
 
     if (userDoc.exists()) {
       await updateDoc(userRef, {
-        draft: arrayUnion({draft:data, platform: platform, date: CurrentDate}),
+        draft: arrayUnion({
+          draft: data,
+          platform: platform,
+          date: CurrentDate,
+        }),
       })
       ;<Alert severity="success">This is a success alert — check it out!</Alert>
     } else {
-      alert("User document not found")
+      alert('User document not found')
     }
   } catch (error) {
-    alert("Error:", error)
+    alert('Error:', error)
     console.log(error)
   }
 }
 
 export const fetchUserDrafts = async (user) => {
-  const userRef = doc(firestore, "users", user.uid)
+  const userRef = doc(firestore, 'users', user.uid)
 
   try {
     const docSnap = await getDoc(userRef)
@@ -130,25 +134,36 @@ export const fetchUserDrafts = async (user) => {
       const userDrafts = userData.draft || []
       return userDrafts
     } else {
-      throw new Error("User document not found")
+      throw new Error('User document not found')
     }
   } catch (error) {
-    throw new Error("Error fetching drafts: " + error.message)
+    throw new Error('Error fetching drafts: ' + error.message)
   }
 }
 
 export const createUserWithEmail = async (email, password) => {
-  const { user } = await createUserWithEmailAndPassword(auth, email, password)
-  if (user.email && user.uid) {
-    const userData = {
-      email: user.email,
-      tokens: 100,
-      model: "text-davinci-002",
-      isNewUser: true,
-      uid: user.uid,
-      drafts: [],
-    }
-    await setDoc(doc(firestore, "users", user.uid), userData)
+  try {
+    await runTransaction(firestore, async (transaction) => {
+      const { user } = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      )
+
+      if (user.email && user.uid) {
+        const userData = {
+          email: user.email,
+          tokens: 100,
+          model: 'text-davinci-002',
+          isNewUser: true,
+          uid: user.uid,
+          drafts: [],
+        }
+        transaction.set(doc(firestore, 'users', user.uid), userData)
+      }
+    })
+  } catch (e) {
+    console.log('failed', e)
   }
 }
 
@@ -157,8 +172,8 @@ export const signInWithEmail = async (email, password) => {
     const { user } = await signInWithEmailAndPassword(auth, email, password)
     return user
   } catch (error) {
-    if (error.code === "auth/user-not-found") {
-      throw new Error("User does not exist")
+    if (error.code === 'auth/user-not-found') {
+      throw new Error('User does not exist')
     } else {
       throw error
     }
@@ -177,8 +192,8 @@ export const signInWithGoogle = async () => {
   const googleProvider = new GoogleAuthProvider()
   const { user } = await signInWithPopup(auth, googleProvider)
 
-  const userRef = collection(db, "users")
-  const q = query(userRef, where("email", "==", user.email))
+  const userRef = collection(db, 'users')
+  const q = query(userRef, where('email', '==', user.email))
 
   const querySnapshot = await getDocs(q)
   let checkUser = []
@@ -191,20 +206,20 @@ export const signInWithGoogle = async () => {
   const userData = {
     email: user.email,
     tokens: 100,
-    model: "text-davinci-002",
+    model: 'text-davinci-002',
     isNewUser: true,
     uid: user.uid,
   }
-  await setDoc(doc(firestore, "users", user.uid), userData, { merge: true })
+  await setDoc(doc(firestore, 'users', user.uid), userData, { merge: true })
   return user
 }
 
 export const onUserSignedIn = async (user, router) => {
-  const userDoc = await doc(firestore, "users", user.uid).get()
+  const userDoc = await doc(firestore, 'users', user.uid).get()
   const isNewUser = userDoc.data().isNewUser
   if (isNewUser) {
-    router.push("/")
+    router.push('/')
   } else {
-    router.push("/")
+    router.push('/')
   }
 }
