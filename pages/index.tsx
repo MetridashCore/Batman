def98d111ad89beededc7560aee098f641979c8e
Mr. Robot <gomexxxx71@gmail.com>
2023-11-27 23:23:32 +0600

    fix: form select input

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,31 @@
 import {
-  Blocks,
-  Growing,
-  Journey,
-  Preview,
-  Prompts,
-  Questions,
-  Testimonials
-} from "@/components/LandingPageComponents/Sections"
-import { auth } from "@/firebase"
-import React, { useEffect } from "react"
+    Blocks,
+    Growing,
+    Journey,
+    Preview,
+    Prompts,
+    Questions,
+    Testimonials,
+} from '@/components/LandingPageComponents/Sections'
+import { auth } from '@/firebase'
+import React, { useEffect } from 'react'
 
 const Home = () => {
-
-
-
-  return (
-    <>
-      <Blocks />
-      <Growing />
-      <Preview />
-      <Prompts />
-      <Testimonials/>
-      <Questions/>
-      <Journey />
-    </>
-  )
+    const theme = localStorage.getItem('theme')
+    if (!theme || theme === 'system') {
+        localStorage.setItem('theme', 'light')
+    }
+    return (
+        <>
+            <Blocks />
+            <Growing />
+            <Preview />
+            <Prompts />
+            <Testimonials />
+            <Questions />
+            <Journey />
+        </>
+    )
 }
 
 export default Home
